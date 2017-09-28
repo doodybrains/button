880cf42d6f1ffaa2f920e45f77d7d4b9fae0bed2
Emma <doodybrainz@gmail.com>
2017-09-28 18:39:31 -0400

    add slack integration

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const dash = dash_button(process.env.button, null, null, 'all');
 
 dash.on("detected", function (){
 	console.log("yesssss");
+
 	axios.get("http://localhost:5005/playlist/%E2%9A%80%20%E2%9A%81%20%E2%9A%82%20D%20I%20C%20E%20%E2%9A%83%20%E2%9A%84%20%E2%9A%85/play").then((sonos) => {
-		setTimeout(() => {
-			axios.get("http://localhost:5005/Rodrigo/pause").then((sonos) => {
-				console.log('Success!')
-			}).catch(error => console.error(error))
-		}, 5000);
+	  setTimeout(() => {
+	    axios.get("http://localhost:5005/Rodrigo/pause").then((sonos) => {
+	      console.log('Success!')
+				axios.post(process.env.slack, {"text": "diiiiiiice", "channel": "studio"
+				 }).then(() => console.log('Successfully sent slack message')).catch(error => console.error(error))
+	    }).catch(error => console.error(error))
+	  }, 5000);
 	}).catch(error => console.error(error))
+
 });
