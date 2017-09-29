2e9d9e374d38159369364b1965b88da3efabc26d
Emma <doodybrainz@gmail.com>
2017-09-29 17:14:12 -0400

    Add env vars

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const axios = require('axios');
 const dash = dash_button(process.env.button, null, null, 'all');
 
 dash.on("detected", function (){
-	console.log("yesssss");
-	axios.get("http://localhost:5005/Rodrigo/volume/50").then(() => 	{
-		console.log('pause');
+	console.log("button detectteeeddd yah000");
+
+	axios.get(process.env.volume).then(() => 	{
+		console.log('volume went up yesssss');
 	}).catch(error => console.error(error))
-	axios.get("http://localhost:5005/Rodrigo/shuffle/on").then(() => 	{
-		console.log('pause');
+	axios.get(process.env.shuffle).then(() => 	{
+		console.log('songs are shuffffledd');
 	}).catch(error => console.error(error))
 
-	axios.get("http://localhost:5005/Rodrigo/playlist/%E2%9A%80%20%E2%9A%81%20%E2%9A%82%20I%20N%20T%20R%20O%20S%20%E2%9A%83%20%E2%9A%84%20%E2%9A%85").then((sonos) => {
+	axios.get(process.env.noises).then((sonos) => {
 	  setTimeout(() => {
-	    axios.get("http://localhost:5005/Rodrigo/playlist/%E2%9A%80%20%E2%9A%81%20%E2%9A%82%20D%20I%20C%20E%20%E2%9A%83%20%E2%9A%84%20%E2%9A%85").then((sonos) => {
-	      console.log('diiiiiidd it.')
-	      axios.post(process.env.slack, {"text": "<!here> :game_die::game_die::game_die:  diiiiiice  :game_die::game_die::game_die:", "channel": "studio"
+	    axios.get(process.env.playlist).then((sonos) => {
+	      console.log('playing playlist')
+	      axios.post(process.env.slack, {"text": process.env.message, "channel": "studio"
 	    }).then(() => console.log('SENT SLACK MESSAGE')).catch(error => console.error(error))
 	    }).catch(error => console.error(error))
 	  }, 4000);
