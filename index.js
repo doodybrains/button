df274ccb72d4a66b489684f0272af708fb68f1a7
Emma <doodybrainz@gmail.com>
2017-09-29 13:38:31 -0400

    Add pause

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const dash = dash_button(process.env.button, null, null, 'all');
 
 dash.on("detected", function (){
 	console.log("yesssss");
+	axios.get("http://localhost:5005/Rodrigo/shuffle/on").then(() => 	{
+		console.log('pause');
+	}).catch(error => console.error(error))
 
-	axios.get("http://localhost:5005/playlist/%E2%9A%80%20%E2%9A%81%20%E2%9A%82%20D%20I%20C%20E%20%E2%9A%83%20%E2%9A%84%20%E2%9A%85/play").then((sonos) => {
+	axios.get("http://localhost:5005/Rodrigo/playlist/%E2%9A%80%20%E2%9A%81%20%E2%9A%82%20I%20N%20T%20R%20O%20S%20%E2%9A%83%20%E2%9A%84%20%E2%9A%85").then((sonos) => {
 	  setTimeout(() => {
-	    axios.get("http://localhost:5005/Rodrigo/pause").then((sonos) => {
-	      console.log('Success!')
-				axios.post(process.env.slack, {"text": "diiiiiiice", "channel": "studio"
-			}).then(() => console.log('SENT SLACK MESSAGE')).catch(error => console.error(error))
+	    axios.get("http://localhost:5005/Rodrigo/playlist/%E2%9A%80%20%E2%9A%81%20%E2%9A%82%20D%20I%20C%20E%20%E2%9A%83%20%E2%9A%84%20%E2%9A%85").then((sonos) => {
+	      console.log('diiiiiidd it.')
+	      axios.post(process.env.slack, {"text": "diiiiiiice", "channel": "studio"
+	    }).then(() => console.log('SENT SLACK MESSAGE')).catch(error => console.error(error))
 	    }).catch(error => console.error(error))
 	  }, 5000);
 	}).catch(error => console.error(error))
